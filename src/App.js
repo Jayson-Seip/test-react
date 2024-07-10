3bb59256bd955287a28702e5f610c3eabecfca38
jayson <jayseip2004@gmail.com>
2024-07-10 09:39:42 -0400

    blank

--- src/App.js
+++ src/App.js
@@ -6,11 +6,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-
-        </p>
-        <p>or Not</p>
         <a
           className="App-link"
           href="https://reactjs.org"
